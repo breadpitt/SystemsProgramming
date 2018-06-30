9f5ac54701796e509da482a889f8081bc44162d2
James Shannon <jashannon10@gmail.com>
2018-06-30 16:06:55 -0600

    countthes works I think except randomly calling ls for some reason

--- countthes.sh
+++ countthes.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
 
+two="2" # silly way to do this probably but new to bash
 infile="alice.txt"
-while read line;do 
+declare -a filearray
+readarray filearray <$infile
+numoflines=0 # 3736 lines total in alice.txt and 568 have two or more thes if I did it right 
+
+while read line; do 
 
 count=$(echo $line | grep -o "the" | wc -l)
-if [$count -gt 1];then
+if [ "$count" -ge "$two" ]; then
 	echo $line
-else
-	echo "NAH BRAH"
+	(( numoflines++ ))
 fi
 done < $infile
+
+echo $numoflines
+
+
+# Probably works but ls gets randomly called every now and then during the program
+# and I'm not sure why
 
 
