4729ee7ea982a59ec0e83d561276c1aa86aa7a0a
James Shannon <jashannon10@gmail.com>
2018-07-23 13:39:50 -0600

    final buggaroos

--- python_stuff/classic_shape.py
+++ python_stuff/classic_shape.py
@@ -11,30 +11,44 @@ class Shape:
     def perimeter(self):
         return reduce(lambda x, y: x + y, self.side_lengths)
 
-    def area(self):
-        pass # Placeholder for virtual area function
+    #def area(self):
+     #   pass # Placeholder for virtual area function
 
 class Square(Shape):
     def __init__(self, side_length, num_sides=4):
         self.num_sides = num_sides
         self.side_lengths = [side_length] * 4
+        self.side_length = side_length
         
     def area(self):   
-        return self.side_lengths * self.side_lengths
+        return self.side_length * self.side_length
 
     def print_shape_details(self):
-        print("Shape has %d sides, perimeter %f, area %f" % (self.num_sides, self.perimeter(), self.area()))
+        print("Shape has {} sides, length {}, perimeter is {}, area {}".format(self.num_sides, self.side_length, self.perimeter(), self.area()))
 
 
 class Rectangle(Shape):
-    def __init__(self, side_length, side_width):
+    def __init__(self, side_length, width_length, num_sides=4):
         self.num_sides = num_sides
+        self.side_lengths = [side_length * 4]
         self.side_length = side_length
-        self.side_width = side_width
-        self.side_length_list = [side_length, side_length, side_width, side_width]
+        self.width_length = width_length
+        self.side_length_list = [side_length, side_length, width_length, width_length]
         
     def area(self):   
-        return self.side_lengths * self.side_width
+        return self.side_length * self.width_length
 
     def print_shape_details(self):
-        print("Shape has {} sides, perimeter {}, area {}".format(self.side_lengths, self.perimeter(), self.area(), self.side_length_list))
+        print("Shape has {} sides, perimeter {}, area {}, side length list is {}".format(self.num_sides, self.perimeter(), self.area(), self.side_length_list))
+
+
+sqlength = float(input("Enter length for the square: "))
+sq = Square(sqlength)
+
+sq.print_shape_details()
+
+length, height = input("Enter length and width for the rectangle: ").split(" ")
+length, height = float(length), float(height)
+rect = Rectangle(length, height)
+
+rect.print_shape_details()
---
+++ python_stuff/grocery_bag.py
@@ -0,0 +1,10 @@
+#!/usr/local/bin/python3
+
+Class GroceryBag:
+    def __init__(self):
+        self.gbag = []
+        self.items = []
+
+    def add_items(self, items):
+        gbag = myitems.append(my_items)
+        return gbag
