58ab5453bf5c08522818e64104f10a120782a367
James Shannon <jashannon10@gmail.com>
2018-08-06 16:30:58 -0600

    monopoly folder created

--- python_stuff/read_csv.py
+++ python_stuff/read_csv.py
@@ -1,10 +1,17 @@
 #!/usr/local/bin/python3
 import mycsv
-path = "pretend_data.csv"
-fh = open(path,'r')
-def main(): 
-    data = mycsv.parse_csv(fh)
-    for i in data:
-        print("Name {} has grade {}".format(i[0], i[1]))
+
+def main():
+    # works but should use with
+    path = "pretend_data.csv"
+    #fh = open(path,'r') 
+    #data = mycsv.parse_csv(fh)
+    #for i in data:
+    #    print("Name {} has grade {}".format(i[0], i[1]))
+
+    with open(path, 'r') as fh:
+        data = mycsv.parse_csv(fh)
+        for i in data:
+            print("Name {} has grade {}".format(i[0], i[1]))
 if __name__ == main():
     main()
